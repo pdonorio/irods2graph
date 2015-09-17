11b53e40933e18b0abdd1e23551299d82f706b8b
paulie <tempo.pause@gmail.com>
2015-09-17 22:30:25 +0200

    Added cypher extension and an example for connection

--- irodsgraph/libs/graph/__init__.py
+++ irodsgraph/libs/graph/__init__.py
@@ -3,6 +3,7 @@
 
 """ Instance of Graph with my current models """
 
+import os
 from libs.graph.graph import GraphDB
 
 ###########################
@@ -12,7 +13,8 @@ host = 'neo'
 port = '7474'
 # username and pw default
 user = 'neo4j'
-pw = user
+#pw = user
+pw = 'test'
 # Connection http descriptor
 GRAPHDB_LINK = \
     protocol + "://" + user + ":" + pw + "@" + host + ":" + port + "/db/data"
@@ -36,15 +38,27 @@ def get_models(lib):
 ################################
 # Saving models inside the graph class, as properties
 
-# Enable OGM models db connection via environment
-import os
-os.environ["NEO4J_REST_URL"] = GRAPHDB_LINK
+def run_from_ipython():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
 
-import libs.graph.ogmmodels
-graph = GraphDB()
-# # This step is automatic
-graph_models = get_models(ogmmodels)
-graph.load_models(graph_models)
-#graph.load_models()
+if run_from_ipython():
+    os.environ["NEO4J_URI"] = GRAPHDB_LINK
+else:
+    # Enable OGM models db connection via environment
+    os.environ["NEO4J_REST_URL"] = GRAPHDB_LINK
 
-# We are ready now
+    import libs.graph.ogmmodels
+    graph = GraphDB()
+    # # This step is automatic
+    graph_models = get_models(ogmmodels)
+    graph.load_models(graph_models)
+    #graph.load_models()
+
+connected = True
+print("Graph connected")
+######################
+# We are ready (graph connected) now
